d447cdfad8f7b356151dccd853807ec89f2f2db0
fung.mak <fung.mak@6317.io>
2023-08-22 16:57:21 +0800

    add pwa

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from "./components/App.tsx";
 
+import './util/register.service.worker'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App/>
